01ad1f7ba372af3f13d5d760410cce32cebf3689
Tommy Palmer <hi@tommyp.org>
2022-12-17 13:13:56 +0000

    generate from an album

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,7 @@
 import { writable } from 'svelte/store';
 
 export const playlistNameStore = writable('');
+
 export const searchResults = writable([]);
 export const notFoundSearchResults = writable([]);
 
